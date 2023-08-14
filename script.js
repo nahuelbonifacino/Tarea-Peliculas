f348fd0cb70b6ceb500639a73134f227edc77645
Federico Trotta <ftrotta18@gmail.com>
2023-08-14 12:30:18 -0300

    cambios para codigo mas leible

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    data.forEach((movie) => {
+const url = "./data.json";
 
-      let div = document.createElement("div");
-      div.classList.add("seccion-1");
+async function fetchMovies() {
+  const response = await fetch(url);
+  // waits until the request completes...
+  const data = await response.json();
+  for (const movie of data) {
+    let div = document.createElement("div");
+    let div2 = document.createElement("div");
+    let img = document.createElement("img");
+    let h1 = document.createElement("h1");
 
-      let img = document.createElement("img");
-      img.src = movie.image;
-     
-      
-      let div2 = document.createElement("div");
-      div2.classList.add("hover");
+    div.classList.add("seccion-1");
+    div2.classList.add("hover");
+    img.src = movie.image;
+    h1.innerHTML = movie.titulo;
 
-      let h1 = document.createElement("h1");
-      h1.innerHTML = movie.titulo
+    div2.appendChild(h1);
+    div.appendChild(img);
+    div.appendChild(div2);
 
-   
-
-      div2.appendChild(h1);
-      div.appendChild(img);
-      div.appendChild(div2);
-
-      document.getElementById("imgA").appendChild(div);
-      
-    });
-  });
+    document.getElementById("imgA").appendChild(div);
+  }
+}
+fetchMovies();
