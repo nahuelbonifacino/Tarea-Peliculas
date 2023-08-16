e9959ee5909458b7fd823b3e607ee940904d0332
Federico Trotta <ftrotta18@gmail.com>
2023-08-16 10:05:29 -0300

    cosas

--- script.js
+++ script.js
@@ -5,29 +5,18 @@ async function fetchMovies() {
   const data = await response.json();
 
   for (const movie of data) {
-    let div = document.createElement("div");
-    let div2 = document.createElement("div");
-    let img = document.createElement("img");
-    let titulo = document.createElement("h1");
-    let anio= document.createElement("h1");
-    let director= document.createElement("h1");
+    const result = ` 
+    <div class="seccion-1">
+      <img src=${movie.image}>
+      <div class="hover">
+        <h1>${movie.titulo}</h1>
+        <h1>${movie.año}</h1>
+        <h1>${movie.director}</h1>
+      </div>
+    </div>
+    `;
 
-    
-    div.classList.add("seccion-1");
-    div2.classList.add("hover");
-    img.src = movie.image;
-    titulo.innerHTML = movie.titulo;
-    anio.innerHTML = movie.año;
-    director.innerHTML = movie.director
-
-    div2.appendChild(titulo);
-    div2.appendChild(anio);
-    div2.appendChild(director);
-
-    div.appendChild(img);
-    div.appendChild(div2);
-
-    document.getElementById("imgA").appendChild(div);
+    document.getElementById("imgA").innerHTML += result;
   }
 }
 fetchMovies();
